9c5ac543aea8296d6414ed716b095bef32a6a56b
Susanne Kolloen <SUKO@equinor.com>
2025-05-27 09:10:12 +0200

    Add some styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,31 +6,31 @@ function App() {
     const [name2, setName2] = useState("");
 
     return (
-        <>
+        <div style={{ width: "20%" }}>
             <TextField
                 label={"Name 1"}
                 id={"name1"}
                 value={name1}
                 onChange={(e: any) => setName1(e.target.value)}
+                style={{ marginBottom: "1rem" }}
             />
             <TextField
                 label={"Name 2"}
                 id={"name2"}
                 value={name2}
                 onChange={(e: any) => setName2(e.target.value)}
+                style={{ marginBottom: "1rem" }}
             />
-            <div>
-                <Button
-                    onClick={() =>
-                        console.log(
-                            `calculating compatibility for ${name1} and ${name2}`
-                        )
-                    }
-                >
-                    Calculate
-                </Button>
-            </div>
-        </>
+            <Button
+                onClick={() =>
+                    console.log(
+                        `calculating compatibility for ${name1} and ${name2}`
+                    )
+                }
+            >
+                Calculate
+            </Button>
+        </div>
     );
 }
 
