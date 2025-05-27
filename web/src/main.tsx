2e980f0fedebf9bd392784c2f2631477bfe27438
Susanne Kolloen <SUKO@equinor.com>
2025-05-27 13:53:56 +0200

    Use flames-game endpoint to return compatability to frontend

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
