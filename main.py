b401309f36e3290a5b033f445e9030006958cdd7
Susanne Kolloen <SUKO@equinor.com>
2025-05-27 13:29:06 +0200

    Allow frontend to connect with api

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from flames_calculator import compatibility
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
